7eeaf98a8a9bc5e9bc6900d98462803481bc3216
jaguar178 <j_j_22@web.de>
2026-04-28 14:37:17 +0200

    Add image prediction function using TensorFlow
    Load a pre-trained model and labels, and implement image prediction functionality.

---
+++ utils/predict.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+
+# Modell laden (einmal!)
+model = tf.keras.models.load_model("model/keras_model.h5", compile=False)
+
+# Labels laden & bereinigen
+with open("model/labels.txt", "r") as f:
+    labels = [line.strip().split(" ", 1)[1] for line in f.readlines()]
+
+def predict_image(image):
+    # Bild vorbereiten
+    img = image.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Prediction
+    predictions = model.predict(img_array)
+    probs = tf.nn.softmax(predictions[0]).numpy()
+
+    index = np.argmax(probs)
+    label = labels[index]
+    confidence = float(probs[index])
+
+    return label, confidence
